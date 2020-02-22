e14c5813c50bb4a43685b2fa8512e49d31a95662
Max Gabrielsson <max.gabrielsson@squeed.com>
2020-02-22 16:21:36 +0100

    Quarkus implementation

---
+++ src/native-test/java/com/gabrielsson/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package com.gabrielsson;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
